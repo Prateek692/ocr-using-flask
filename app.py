34ec89c41bc017084096d5915a4611a37a6b8cf2
Prateek <prateek692@hotmail.com>
2023-11-08 09:29:49 +0530

    Implemented textfile download mechanism

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os  
-from flask import Flask, flash, redirect, render_template, request, session, url_for
+from flask import Flask, flash, redirect, render_template, request, session, url_for,send_file
 # import our OCR function
 from ocr_core import ocr_core
 
@@ -15,7 +15,11 @@ app = Flask(__name__)
 def allowed_file(filename):  
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
+# function to save text file
+def txt(str):
+    text_file = open('parsed_text.txt', 'w')
+    text_file.write(str)
+    text_file.close()
 # route and function to handle the home page
 @app.route('/')
 def home_page():  
@@ -33,13 +37,20 @@ def upload_page():
         if file.filename == '':
             return render_template('new_page.html', msg='No file chosen!')
         if file and allowed_file(file.filename):
+            # Retrieve desired output format from form
+            output_format=request.form.get('option')
+            if output_format=='Select output format':
+                return render_template('new_page.html', msg='Choose valid output format!')
             # call the OCR function on it
             extracted_text = ocr_core(file)
-            # extract the text and display it
-            return render_template('upload.html',
-                                   msg='Successfully processed',
-                                   extracted_text=extracted_text,
-                                   img_src=UPLOAD_FOLDER + file.filename)
+            if output_format=='JSON':
+                return render_template('new_page.html', msg='JSON')
+            elif output_format=='Plaintext':
+                return render_template('new_page.html', msg='Text File')
+            else:
+                txt(extracted_text)
+                path = 'parsed_text.txt'
+                return send_file(path, as_attachment=True)
     elif request.method == 'GET':
         return render_template('new_page.html',msg='Hey yo')
 
